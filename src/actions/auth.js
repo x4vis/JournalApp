f28cdd1c3b82ef242a1cd0501c080d8538b08c5d
Javier Soto <javislop@gmail.com>
2021-02-23 21:51:20 -0600

    add alerts to show errors, and change async await instead of then

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,47 +1,47 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from "../types/types";
 import { finishLoading, startloading } from './ui';
+import Swal from 'sweetalert2';
 
 //action que se ejecuta cuando es un login sin alguna red social o cuenta
 export const startLoginEmailPassword = (email, password) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     //cambia el state de loading a true
     dispatch(startloading());
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(({ user }) => {
+
+    try {
+      const user = await firebase.auth().signInWithEmailAndPassword(email, password);
       //se setea el estado para el login y se cambia el estado
       //de loading a false
       dispatch(login(user.uid, user.displayName));
       dispatch(finishLoading());
-    })
-    .catch( err => {
-      console.log(err);
+    } catch (err) {
+      Swal.fire('Error', err.message, 'error');
       //se cambia el estado de loading a false
       dispatch(finishLoading());
-    });
+    }
   }
 }
 
 export const startRegisterWithEmailPasswordName = (email, password, name) => {
-  return (dispatch) => {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then( async ({ user }) => {
+  return async (dispatch) => {
+    try {
+      const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
       //actualizacion de perfil para hacer set del displayName con el name de
       //del usuario que se esta registrando, de otra forma devuelve vacio este campo
       await user.updateProfile({ displayName: name });
       //se hace el dispatch del login despues del registro para redux
       dispatch(login(user.uid, user.displayName))
-    })
-    .catch(error => console.log(error))
+    } catch (err) {
+      Swal.fire('Error', err.message, 'error');
+    }
   }
 }
 
 export const startGoogleLogin = () => {
-  return (dispatch) => {
-    firebase.auth().signInWithPopup(googleAuthProvider)
-    .then(({ user }) => {
-      dispatch(login(user.uid, user.displayName))
-    });
+  return async (dispatch) => {
+    const user = await firebase.auth().signInWithPopup(googleAuthProvider);
+    dispatch(login(user.uid, user.displayName));
   }
 }
 
