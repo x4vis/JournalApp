c2eeaba4d49bbea33b49744b29db1b86bf391a93
Javier Soto <javislop@gmail.com>
2021-03-04 11:14:38 -0600

    add animate classes to containers

--- src/components/journal/JournalEntry.jsx
+++ src/components/journal/JournalEntry.jsx
@@ -13,7 +13,7 @@ export const JournalEntry = ({ id, date, title, body, url }) => {
   }
 
   return (
-    <div className="journal__entry"
+    <div className="journal__entry animate__animated animate__fadeIn animate__faster"
          onClick={handleActiveNote}>
 
       {
